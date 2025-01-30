e28775062be52abfe7102598bebe0ae7b75466f0
nhimsel <nathanael.himsel@cuw.edu>
2025-01-30 08:31:09 -0600

    connect all rooms

--- Driver.java
+++ Driver.java
@@ -16,7 +16,13 @@ public class Driver
 	    r1.addExit("north", r2);
 	    r2.addExit("north", r3);
 	    r2.addExit("south", r1);
-	    
+	    r3.addExit("north", r6);
+        r3.addExit("south", r2);
+        r3.addExit("west", r4);
+        r3.addExit("east", r5);
+        r4.addExit("east", r3);
+        r5.addExit("west", r3);
+        r6.addExit("south", r3);
 
         /*...add the rest of the exits to all rooms
 	    ...the make each room display itself showing something like:
